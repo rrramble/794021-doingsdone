ccee632575b5585de0c26e28b1163a2ea5692b8e
rrramble <ru7ar7@gmail.com>
2019-02-10 08:27:18 +0500

    fix: tune mail and username length, comments added

--- schema.sql
+++ schema.sql
@@ -21,10 +21,28 @@ CREATE TABLE tasks (
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   date_created TIMESTAMP NOT NULL,
-  email VARCHAR(200) UNIQUE NOT NULL,
-  name VARCHAR(200) NOT NULL,
-  password_hash VARCHAR(255) NOT NULL
+  email VARCHAR(254) UNIQUE NOT NULL,
+  name VARCHAR(68) NOT NULL,
+  password_hash VARCHAR(266) NOT NULL
 );
+
+/*  About email length
+    Although name part may be up to 64 and domain's up to 255,
+    there is a restriction of mailbox: 256, including '<' and '>'
+    which gives 254 = 256 - 2 character long email fields.
+    See:
+    https://blog.moonmail.io/what-is-the-maximum-length-of-a-valid-email-address-f712c6c4bc93
+ */
+
+/*  About name length
+    Name might be constited of first, second (patronimic), and last.
+    Mastercard limits first and second up to 22.
+    My consideration is to 22 + 22 + 22 = 66,
+    multiplied to UTF's length of 4,
+    plus 2 spaces between the parts.
+    So: (66 * 4) + 2 * 1 = 266
+ */
+
 
 CREATE TABLE statuses (
   id INT PRIMARY KEY,
