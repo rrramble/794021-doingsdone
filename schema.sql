79df3136a8f3ee4476e73c14e0a8dd493c524f3a
rrramble <ru7ar7@gmail.com>
2019-02-11 23:26:26 +0500

    feature: add 'date_created' to 'projects' table

--- schema.sql
+++ schema.sql
@@ -7,7 +7,8 @@ USE 794021_doingsdone;
 CREATE TABLE projects (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(60) NOT NULL,
-  author_user_id INT NOT NULL
+  author_user_id INT NOT NULL,
+  date_created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE project_categories (
