3edaa6b005c9317b7a39bed56abc1f4e673b1821
rrramble <ru7ar7@gmail.com>
2019-02-27 02:17:41 +0500

    feature: remove task hardcoded in php

--- templates/index.php
+++ templates/index.php
@@ -59,21 +59,5 @@
             <?php endif; ?>
         <?php endforeach; ?>
 
-        <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
-        <?php if ($show_complete_tasks === 1) : ?>
-            <tr class="tasks__item task task--completed">
-                <td class="task__select">
-                    <label class="checkbox task__checkbox">
-                        <input class="checkbox__input visually-hidden" type="checkbox" checked>
-                        <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
-                    </label>
-                </td>
-                <td class="task__date">10.10.2019</td>
-
-                <td class="task__controls">
-                </td>
-            </tr>
-        <?php endif; ?>
-
     </table>
 </main>
