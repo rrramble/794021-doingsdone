eb23d29bfd3254efee77d671923f17ffec22628d
Rustam <ru7ar7@gmail.com>
2019-03-06 14:18:17 +0500

    style: reindent spaces

--- pages/register-form.php
+++ pages/register-form.php
@@ -2,44 +2,42 @@
 include_once('../abstract-form.php');
 
 class RegisterForm extends AbstractForm {
-        function __construct()
-        {
-            $this->Field['email'] = [
-                'formTagName' => 'email',
-                'isPublic' => true,
-                'validationCb' => function() {
-                    $value = $this->getValue('email');
-                    return (boolean)filter_var($value, FILTER_VALIDATE_EMAIL);
-                }
-            ];
+    function __construct()
+    {
+        $this->Field['email'] = [
+            'formTagName' => 'email',
+            'isPublic' => true,
+            'validationCb' => function() {
+                $value = $this->getValue('email');
+                return (boolean)filter_var($value, FILTER_VALIDATE_EMAIL);
+            }
+        ];
 
-            $this->Field['password'] = [
-                'formTagName' => 'password',
-            ];
+        $this->Field['password'] = [
+            'formTagName' => 'password',
+        ];
 
-            $this->Field['passwordHash'] = [
-                'isPublic' => true,
-            ];
+        $this->Field['passwordHash'] = [
+            'isPublic' => true,
+        ];
 
-            $this->Field['userName'] = [
-                'formTagName' => 'name',
-                'isPublic' => true,
-                'validationCb' => function() {
-                    $value = $this->getValue('userName');
-                    return mb_strlen($value) > 0;
-                }
-            ];
+        $this->Field['userName'] = [
+            'formTagName' => 'name',
+            'isPublic' => true,
+            'validationCb' => function() {
+                $value = $this->getValue('userName');
+                return mb_strlen($value) > 0;
+            }
+        ];
 
-            parent::__construct();
+        parent::__construct();
 
-            if ($this->isMethodPost()) {
-                $this->Field['passwordHash']['value'] = password_hash(
-                    $this->getValue('password'),
-                    PASSWORD_DEFAULT
-                );
-            };
-        }
+        if ($this->isMethodPost()) {
+            $this->Field['passwordHash']['value'] = password_hash(
+                $this->getValue('password'),
+                PASSWORD_DEFAULT
+            );
+        };
+    }
 
-    } // class RegisterForm
-
-    ?>
+} // class RegisterForm
