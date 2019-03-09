89277cb03f97bf2cc756903a7278b755e25840f7
rrramble <ru7ar7@gmail.com>
2019-03-10 01:24:47 +0600

    fix: link to 'add.php' instead of .html file

--- templates/layout.php
+++ templates/layout.php
@@ -73,7 +73,7 @@
                     </nav>
 
                     <a class="button button--transparent button--plus content__side-button"
-                    href="pages/form-project.html" target="project_add">Добавить проект</a>
+                    href="/pages/add.php" target="project_add">Добавить проект</a>
                 </section>
 
                 <?= $data["components"]["main"]; ?>
