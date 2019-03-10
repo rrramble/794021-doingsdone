7df55670baed46a668ac2b9dd41147a6be942859
rrramble <ru7ar7@gmail.com>
2019-03-10 17:09:13 +0500

    fix: correct link to 'add-task.php' instead of 'add.php'

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,7 @@
     </a>
 
     <div class="main-header__side">
-        <a class="main-header__side-item button button--plus open-modal" href="/pages/add.php">Добавить задачу</a>
+        <a class="main-header__side-item button button--plus open-modal" href="/pages/add-task.php">Добавить задачу</a>
 
         <div class="main-header__side-item user-menu">
             <div class="user-menu__image">
