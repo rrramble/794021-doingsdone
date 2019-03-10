7f22ffb959e18609bf2b568552ef2d558471ed0f
rrramble <ru7ar7@gmail.com>
2019-03-10 19:31:31 +0500

    feature: include JS-scripts in the layout.php

--- templates/layout.php
+++ templates/layout.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="/css/flatpickr.min.css">
 </head>
 
+<body>
 <h1 class="visually-hidden">Дела в порядке</h1>
 
 <div class="page-wrapper">
@@ -112,3 +113,6 @@
         </div>
     </div>
 </footer>
+<script src="/flatpickr.js"></script>
+<script src="/script.js"></script>
+</body>
