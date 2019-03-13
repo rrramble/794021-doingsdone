7e52362632aec7496aba5977b3d6a722068aa42d
rrramble <ru7ar7@gmail.com>
2019-03-14 03:18:10 +0500

    style: remove unused var '$SCRIPT_NAME_IF_FAILURE'

--- pages/register.php
+++ pages/register.php
@@ -5,7 +5,6 @@ include_once('register-form.php');
 
 const WEBPAGE_TITLE = 'Регистрация пользователя';
 const SCRIPT_NAME_IF_SUCCESS = '/pages/auth.php';
-$SCRIPT_NAME_IF_FAILURE = 'register.php';
 const FormMessage = [
     'OVERALL_ERROR' => 'Пожалуйста, исправьте ошибки в форме',
     'EMAIL_IS_EMPTY' => 'Указать электронную почту',
