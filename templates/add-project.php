e94737dbae3e80da7a9e88d335ef35063b85d1a6
rrramble <ru7ar7@gmail.com>
2019-03-15 02:00:02 +0500

    fix: use isset and make local vars from checked data

--- templates/add-project.php
+++ templates/add-project.php
@@ -5,6 +5,11 @@ if (!isset($data)) {
 };
 
 $CLASS_INPUT_ERROR = 'form__input--error';
+
+$postTaskTitle = $data["postTaskTitle"] ?? "";
+$taskTitleInvalidMessage = $data["taskTitleIvalidMessage"] ?? "";
+$isTitleValid = (boolean)($data['isTitleValid'] ?? true);
+
 ?>
 <h2 class="content__main-heading">Добавление проекта</h2>
 
@@ -13,14 +18,14 @@ $CLASS_INPUT_ERROR = 'form__input--error';
         <label class="form__label" for="project_name">Название <sup>*</sup></label>
 
         <input
-            class="form__input <?= !$data['isTitleValid'] ? $CLASS_INPUT_ERROR : ""; ?>"
+            class="form__input <?= !$isTitleValid ? $CLASS_INPUT_ERROR : ""; ?>"
             type="text" name="name" id="project_name"
-            value="<?= strip_tags($data["postTaskTitle"]); ?>"
+            value="<?= strip_tags($postTaskTitle); ?>"
             placeholder="Введите название проекта">
 
-        <?php if(!$data['isTitleValid']): ?>
+        <?php if(!$isTitleValid): ?>
             <p class="form__message">
-            <?= strip_tags($data["taskTitleIvalidMessage"]); ?>
+            <?= strip_tags($taskTitleInvalidMessage); ?>
             </p>
         <?php endif; ?>
 
