6b1f5907729c85c0ede70785e7d031e2090bd527
Rustam <ru7ar7@gmail.com>
2019-03-14 15:41:15 +0500

    feature: add module to search for task titels (known bug: search is not made)

--- templates/index.php
+++ templates/index.php
@@ -15,7 +15,9 @@ $HtmlClasses = [
     <h2 class="content__main-heading">Список задач</h2>
 
     <form class="search-form" action="index.php" method="post">
-        <input class="search-form__input" type="text" name="task-search" value="" placeholder="Поиск по задачам">
+        <input class="search-form__input" type="text" name="task-search"
+            value="<?= isset($data["postTitle"]) ? strip_tags($data["postTitle"]) : ""; ?>" placeholder="Поиск по задачам"
+        >
 
         <input class="search-form__submit" type="submit" name="" value="Искать">
     </form>
