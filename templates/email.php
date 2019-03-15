e528f5ad2b3d43006def6ba3d3595d542e70c13b
Rustam <ru7ar7@gmail.com>
2019-03-15 18:33:34 +0500

    feature: add template 'email.php' for message body

---
+++ templates/email.php
@@ -0,0 +1,34 @@
+<?php
+
+if (
+    !isset($tasks) ||
+    !isset($user) ||
+    count($tasks) <= 0
+) {
+    header("Location: /");
+    die();
+};
+
+$userName = $user["userName"] ?? "";
+
+?>
+<p>Уважаемый, <?= strip_tags($userName); ?>.</p>
+<p>У вас запланированы задачи:</p>
+
+<table border="1">
+    <tr>
+        <th>Название:</th>
+        <th>Дата/время:</th>
+    </tr>
+
+    <?php foreach($tasks as $task): ?>
+        <?php
+            $title = $task["title"] ?? "";
+            $date = $task["dueDate"] ?? "";
+        ?>
+        <tr>
+            <td><?= strip_tags($title); ?></td>
+            <td><?= strip_tags($date); ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
